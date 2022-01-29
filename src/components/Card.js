8a2f19935acdc04d603d3f623e9925c1720a096c
Anna N <csofenn@gmail.com>
2022-01-29 13:30:18 +0000

    Create Detail and Avatar components to split the App into even smaller components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Avatar from './Avatar';
+import Detail from './Detail';
 
 //Creating a 'model' card so we can use App to generate dynamic card with different information
 function Card(props) {
@@ -7,15 +9,11 @@ function Card(props) {
         <div className="card">
           <div className="top">
             <h2 className="name">{props.name}</h2>
-            <img
-              className="circle-img"
-              src={props.img}
-              alt="avatar_img"
-            />
+            <Avatar img={props.img}/>
           </div>
           <div className="bottom">
-            <p>{props.phone}</p>
-            <p>{props.email}</p>
+            <Detail detail={props.email}/>
+            <Detail detail={props.phone}/>
           </div>
         </div>
      
---
+++ src/components/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Detail(props){
+    return(
+      
+            <p>{props.detail}</p>
+            
+        
+    )
+}
+
+export default Detail;
